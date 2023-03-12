f2a402ad97982a5d0f15baace2e66fd0342aad87
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-03-12 12:17:46 +0000

    constructor

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 let employees = [];
 function EmployManagmantSystem(EmployeeID,FullName,Department,Level,ImagrURL){
-    this.EmployeeID = EmployeeID;
-    this.FullName = FullName;
-    this.Department = Department;
-    this.Level = Level;
-    this.ImagrURL = ImagrURL;
+    this.employeeID = EmployeeID;
+    this.fullName = FullName;
+    this.department = Department;
+    this.level = Level;
+    this.imagrURL = ImagrURL;
+    this.salary=this.NetSalary;
     employees.push(this);
+    
 }
 
 let GhaziSamer = new EmployManagmantSystem (1000,'GhaziSamer','Administration','Senior') ;
@@ -17,34 +19,46 @@ let OmarZaid = new EmployManagmantSystem (1004,'OmarZaid','Development','Senior'
 let RanaSaleh = new EmployManagmantSystem (1005,'RanaSaleh','Development','Junior');
 let HadiAhmad = new EmployManagmantSystem (1006,'HadiAhmad','Finance','Mid-Senior');
 
-EmployManagmantSystem.prototype.Salary = function () {
-if(this.Level== "Senior"){
+EmployManagmantSystem.prototype.salaryCal = function () {
+    let s1;
+if(this.Level== 'Senior'){
     let min=1500; let max=2000;
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return  (Math.floor(Math.random() * (max - min) ) + min); 
+    
 }else if(this.Level == 'Mid-Senior'){
     let min=1000; let max=1500;
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return  (Math.floor(Math.random() * (max - min) ) + min); 
+    
 }else if(this.Level == 'Junior'){
     let min=500; let max=1000;
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return  (Math.floor(Math.random() * (max - min) ) + min);     
 }
 }
-EmployManagmantSystem.prototype.NetSalary = function () {
-return this.Salary()*0.925;
+
+EmployManagmantSystem.prototype.NetSalary =function() {
+return this.salaryCal*0.925;
 }
 
 EmployManagmantSystem.prototype.render = function () {
-document.write(`Name:${this.FullName} `);
-document.write(`Salary = ${this.Salary()} `); 
-document.write(`Net Salary = ${this.NetSalary()}`)
+document.write(`Name:${this.fullName} & Salary = ${this.salary} `);
+// document.write(`Salary = ${this.salary} `); 
+// document.write(`Net Salary = ${this.NetSalary()}`)
 }
 
 function employeesDetails (){
 for(let i=0 ; i<employees.length ; i++){
 
+    employees[i].NetSalary();
     employees[i].render();
+    
+    
+    
 
 }
 }
 
 employeesDetails();
+// GhaziSamer.salaryCal();
+// GhaziSamer.render();
+// let min=1500; let max=2000;
+// console.log((Math.floor(Math.random() * (max - min) ) + min));
